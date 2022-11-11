f19cce044298b2bfbc275b987e0ef7d17ebbe19a
hg0428 <45984899+hg0428@users.noreply.github.com>
2022-11-11 20:53:44 +0000

    bug fixes, improvements

--- README.md
+++ README.md
@@ -8,3 +8,9 @@ You can use this model to generate code for you.
 Adjust the values in generate.py to optimize the AI for what you need. If you have enough storage, try loading one of the bigger models. 
 
 Training is currently not available, but I will update this template as soon as I figure that out.
+
+### Github
+https://github.com/hg0428/Codegen-DEMO
+
+### If you enjoyed this:
+https://discord.com/invite/KqhqnHrrZJ
--- main.py
+++ main.py
@@ -4,32 +4,42 @@
 #source and data: https://github.com/salesforce/CodeGen
 from sty import fg, bg
 from generate import generate
-
-
-
-
-
-
+from os import listdir
 
 # Code to load the demos.
 print(
   f'{fg(0, 250, 255)}\nTHIS IS A DEMO running from a smaller dataset. {fg.rs}\nA larger dataset may provide better results.\nType {bg(0, 0, 0)+fg(255, 75, 100)}example {fg(255, 255, 0)}[file]{bg.rs+fg.rs} to complete a demo file.\nExample: {bg(0, 0, 0)+fg(255, 75, 100)}example {fg(255, 255, 0)}example.py{bg.rs+fg.rs}\n'
 )
 
-
 while True:
   cmd = input('$ ').split()
-  
+
   if cmd[0] == 'example':
-    with open('examples/' + cmd[1]) as f:
-      output = generate(f.read())
-      print("Output:\n", output['fullcode'], sep="")
-      print(f"Generation took {output['time']} seconds.")
-      
+    print("Loading...")
+    try:
+      with open('examples/' + cmd[1]) as f:
+        output = generate(f.read())
+        print(f"Output:\n{bg(0, 0, 0)+fg(190, 190, 255)}",
+              output['fullcode'],
+              fg.rs + bg.rs,
+              sep="")
+        print(f"Generation took {output['time']} seconds.")
+    except:
+      print("Could not find example file.")
+
   if cmd[0] == 'complete':
-    ouput = generate(' '.join(cmd[1:]))
-    print("Output:\n", output['fullcode'], sep="")
+    print("Loading...")
+    output = generate(' '.join(cmd[1:]))
+    print(f"Output:\n{bg(0, 0, 0)+fg(190, 190, 255)}",
+          output['fullcode'],
+          fg.rs + bg.rs,
+          sep="")
     print(f"Generation took {output['time']} seconds.")
-    
+
+  if cmd[0] == 'list':
+    print('\n'.join(listdir('examples/')))
+
   if cmd[0] == 'help':
-    print('$example [file] — Uses AI to autocomplete.\n$complete [code] — Autocompletes the inputted code.')
+    print(
+      'example [file] — Uses AI to autocomplete.\ncomplete [code] — Autocompletes the inputted code.\nlist — Lists all available demos.'
+    )
